4e211d48589d578bc6bbcdee42b7781884e08de4
ignacio005 <ignacio.tiradomeza@usp.ceu.es>
2024-02-14 09:28:41 +0100

    javadoc

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -16,7 +16,7 @@ import mates.Matematicas;
 
 public class Principal {
     public static void main(String[] args){
-        System.out.println("El número PI es " + Matematicas.generarNumeroPiIterativo(Integer.parseInt(args[0])));
+        System.out.println("El número PI es " + Matematicas.generarNumeroPiIterativo(Integer.parseInt("10000")));
     }
 
 }
